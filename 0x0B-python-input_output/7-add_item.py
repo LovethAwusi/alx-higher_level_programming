4cc832464e4b80801adbf10a95ee42508a02dca5
LovethAwusi <awusieloho@gmail.com>
2023-12-05 03:07:07 -0800

    Second commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-
+""" adds all arguments to a Python
+    list, and then save them to a file:
 """
-This module contains script that adds all arguments to a Python list
-and save them to a file.
+import sys
+import os
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
-module save_to_json_file: serializes the object i.e convert to JSON string
-module load_to_json_file: deserializes the JSON string i.e convert back to
-object.
-"""
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def add_item(args, filename):
+    if (os.path.exists(filename)):
+        content = load_from_json(filename)
+    else:
+        content = []
+    content.extend(args)
+    save_to_json(content, filename)
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
 
-save_to_json_file(my_list + argv[1:], "add_item.json")
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
